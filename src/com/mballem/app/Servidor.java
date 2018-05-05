f0d82c5a7b2edb33ed2e61f097c15336cac9fec0
marcos <marcos@Vostro-3450>
2018-05-05 08:42:32 -0300

    Main separado

---
+++ src/com/mballem/app/Servidor.java
@@ -0,0 +1,9 @@
+package com.mballem.app;
+import com.mballem.app.service.ServidorService;
+
+public class Servidor {
+
+    public static void main(String[] args) {
+        new ServidorService();
+    }
+}
